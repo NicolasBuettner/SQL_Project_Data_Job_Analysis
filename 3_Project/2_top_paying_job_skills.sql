320c3f5dae115f08776b6807b4574360b378015a
Nicolas Buettner <nicolas.buettner@gmx.ch>
2025-10-02 16:32:00 +0200

    task 3

--- 3_Project/2_top_paying_job_skills.sql
+++ 3_Project/2_top_paying_job_skills.sql
@@ -13,7 +13,8 @@ WITH top_paying_jobs AS (
         name AS company_name
     FROM
         job_postings_fact
-    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    LEFT JOIN 
+        company_dim ON job_postings_fact.company_id = company_dim.company_id
     WHERE
         job_title_short = 'Data Analyst'
         AND salary_year_avg IS NOT NULL
@@ -31,9 +32,9 @@ SELECT
     skills
 FROM
     top_paying_jobs
-	INNER JOIN
+INNER JOIN
     skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
-	INNER JOIN
+INNER JOIN
     skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 ORDER BY
     salary_year_avg DESC;
